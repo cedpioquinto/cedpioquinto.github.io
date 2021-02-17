09fdd11e47fff24b6f6cc0a56e97745f4cafa3dd
cedpioquinto <cjpioqs@gmail.com>
2021-02-17 15:10:19 +0800

    Week 06 Calculator

--- footwear.js
+++ footwear.js
@@ -5,18 +5,21 @@ function footwear(){
     let weather = document.getElementById("weather").value
 
 
-//Input: user assigned info to specified footwear
+//PROCESSING: user assigned info to specified footwear
     if (weather=="hot"){footwear="Sandals";}
     else if(weather=="rain"){footwear="Galoshes";}
     else if(weather=="snow"){footwear="Boots";}
     else{footwear="Shoes";}
 
-    document.getElementById('output').innerHTML = footwear
+    //OUTPUT: Displays the appropriate footwear for the weather
+    document.getElementById('output').innerHTML =  'I highly recommend you to wear ' +  footwear + '.';
 }
+
+
+
     
         
     
-
 
 
 
