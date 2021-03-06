22dd2747a70592e9dc1811916e9385ad816e0a7a
cedpioquinto <cjpioqs@gmail.com>
2021-03-06 14:16:26 +0800

    repetition

--- fiveTimes.js
+++ fiveTimes.js
@@ -1,9 +1,9 @@
 //create a sequence five times multiplied up to 12 format
 let output = '';
-let result = 0;
+let i = 1;
 
 //Processing: calculate 12 times 
-for (let i = 1; i <= 12; i++) {    
+for (; i <= 12; i++) {    
     result = 5 * i;
     // OUTPUT: Shows the result
     output += `5 × ${i} = ${result}<br>`;}
